3e0b9b0e19f4c7d940a970313490f2bf21af66fc
Alvaro <alvaro.martinez@yudonpay.com>
2020-04-23 21:43:29 +0200

    add mongoose

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const path = require('path');
 const inert = require('inert');
 const vision = require('@hapi/vision');
 
+//database
+require('./database')
+const User = require('./models/user')
+
 const init  = async () => {
   const server = new hapi.Server({
     port: 3000,
@@ -23,7 +27,7 @@ const init  = async () => {
     },
     relativeTo: __dirname,
     path: 'templates',
-    isCached: false
+    isCached: process.env.NODE_ENV === 'prod'
   });
 
   await server.start();
@@ -36,6 +40,52 @@ const init  = async () => {
       return h.view('index')
     }
   });
+
+  server.route({
+    method:'GET',
+    path:'/name',
+    handler: (request, h) => {
+      return h.view('namepage', {
+        name: 'Alvaro'
+      })
+    }
+  })
+  server.route({
+    method:'GET',
+    path:'/products',
+    handler: (request, h) => {
+      return h.view('products', {
+          products: [
+            {name: 'laptop'},
+            {name: 'mouse'},
+            {name: 'keyword'},
+            {name: 'monitor'},
+          ]
+      })
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/users',
+    handler: async (request, h) => {
+      const users = await User.find();
+      console.log(users)
+      return h.view('users', {
+        users
+      })
+    }
+  })
+
+  server.route({
+    method: 'POST',
+    path: '/users',
+    handler: (request, h) => {
+      const newUser = new User({username: request.payload.username})
+      console.log(newUser)
+      return h.redirect().location('users')
+    }
+  })
 };
 
 init();
